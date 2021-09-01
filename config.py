c0e5328665e343d9d2bf026c88cbea2c3518f648
Maciej Czarkowski <mczarkowski98@gmail.com>
2021-09-01 21:47:22 +0200

    Add sample vue js app and tailwind css config

--- config.py
+++ config.py
@@ -1,19 +1,9 @@
 import os
 
+
 class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    DUSZYN = os.environ.get("DUSZYN")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    TWITTER_OAUTH_CLIENT_KEY = os.environ.get("TWITTER_OAUTH_CLIENT_KEY")
-    TWITTER_OAUTH_CLIENT_SECRET = os.environ.get("TWITTER_OAUTH_CLIENT_SECRET")
-    SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = 'None'
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
+    MAIL_PORT = 6969
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
@@ -21,14 +11,16 @@ class Config(object):
 
 
 class ProductionConfig(Config):
-    DEBUG = False
-    SECRET_KEY = "9asdf8980as8df9809sf6a6ds4f3435fa64ˆGggd76HSD57hsˆSDnb"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = os.environ.get("DEBUG")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_USERNAME = os.environ.get("DB_USERNAME")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 
 class DevelopmentConfig(Config):
-    ENV = "development"
-    DEVELOPMENT = True
-    SECRET_KEY = "secret_for_test_environment"
-    OAUTHLIB_INSECURE_TRANSPORT = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG = os.environ.get("DEBUG")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    MYSQL_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_USERNAME = os.environ.get("MYSQL_USERNAME")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
+from dotenv import load_dotenv
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.environ['CONFIGURATION_SETUP'])
+mysql = MySQL(app)
+CORS(app, resources={r'/*': {'origins': '*'}})
+
 
 @app.route('/')
 def index():
-    return 'Hello World' + str(app.config['MAIL_PORT'])
+    return render_template('home.html', template_folder="templates")
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    print(request.form)
+    return "dupa"
+
 
 if __name__ == "__main__":
     app.run()
