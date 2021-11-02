8e034112812c3d74f4012a3ec624cd10d697a155
Maciej Czarkowski <mczarkowski98@gmail.com>
2021-11-02 15:39:52 +0100

    Add data from yellow labs, enable proxies for requests

--- parseWebsitesFromFile.py
+++ parseWebsitesFromFile.py
@@ -1,5 +1,6 @@
 from websiteParser import parse_website_js, fill_database
 import mysql.connector
+import traceback
 
 
 mydb = mysql.connector.connect(
@@ -15,12 +16,18 @@ def get_cursor():
 
 
 with open('websites.txt', 'r') as websites_file:
+    cursor = get_cursor()
     for line in websites_file:
         try:
-            print(f"Parsing url: {line}")
+            print(f"Parsing url: {line.strip()}")
+            cursor.execute(''' SELECT COUNT(*) FROM websites WHERE url = %s''', (line.strip(),))
+            result = cursor.fetchall()
+            if result[0]['COUNT(*)'] > 0:
+                print("Url already in database")
+                continue
             scrapping_result_dictionary = parse_website_js(line.rstrip())
             print(scrapping_result_dictionary)
             fill_database(scrapping_result_dictionary, get_cursor(), mydb)
         except Exception as e:
-            print(f"Error occured while parsing url: {line}, error: {e}")
+            print(f"Error occured while parsing url: {line.strip()}, error: {e}, stacktrace: {traceback.format_exc()}")
 
--- websiteParser.py
+++ websiteParser.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup, NavigableString
 from requests_html_custom import request_html
 import MySQLdb
 import re
+import requests
+import json
+import time
 
 
 def parse_website_js(url):
@@ -42,6 +45,8 @@ def parse_website_js(url):
         soup_count = soup.find_all(tag)
         scrapping_results_dictionary[tag] = len(soup_count)
     session.close()
+
+    parse_website_with_yellow_lab_tools(url, scrapping_results_dictionary)
     return scrapping_results_dictionary
 
 
@@ -54,8 +59,12 @@ def fill_database(result_dictionary, cursor, connection):
     cursor.execute(''' INSERT INTO websites 
     (url, h1_count, h2_count,  div_count, iframe_count, a_count, img_count, p_count, span_count, button_count,
      input_count, form_count, script_count, meta_count, link_count, style_count, text_longer_than_100_characters_count,
-     http_references, https_references, total_code_length)
-     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+     http_references, https_references, total_code_length, ylt_DOMelementsCount, ylt_DOMelementMaxDepth,
+      ylt_scriptDuration, ylt_jsErrors, ylt_cssColors, ylt_cssDuplicatedProperties, ylt_totalWeight, ylt_domains,
+       ylt_compression, ylt_totalRequests, ylt_heavyFonts, ylt_global_score, ylt_badJavascript_score, ylt_jQuery_score,
+        ylt_serverConfig_score)
+     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+     %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
                    (result_dictionary['url'], result_dictionary['h1'], result_dictionary['h2'], result_dictionary['div'],
                     result_dictionary['iframe'], result_dictionary['a'], result_dictionary['img'],
                     result_dictionary['p'], result_dictionary['span'],
@@ -63,10 +72,58 @@ def fill_database(result_dictionary, cursor, connection):
                     result_dictionary['script'], result_dictionary['meta'], result_dictionary['link'],
                     result_dictionary['style'], result_dictionary['text_longer_than_100_characters_count'],
                     result_dictionary['http_references'], result_dictionary['https_references'],
-                    result_dictionary['total_code_length']))
+                    result_dictionary['total_code_length'], result_dictionary['ylt_DOMelementsCount'],
+                    result_dictionary['ylt_DOMelementMaxDepth'], result_dictionary['ylt_scriptDuration'],
+                    result_dictionary['ylt_jsErrors'], result_dictionary['ylt_cssColors'],
+                    result_dictionary['ylt_cssDuplicatedProperties'], result_dictionary['ylt_totalWeight'],
+                    result_dictionary['ylt_domains'], result_dictionary['ylt_compression'],
+                    result_dictionary['ylt_totalRequests'], result_dictionary['ylt_heavyFonts'],
+                    result_dictionary['ylt_global_score'], result_dictionary['ylt_badJavascript_score'],
+                    result_dictionary['ylt_jQuery_score'], result_dictionary['ylt_serverConfig_score']))
     connection.commit()
 
     cursor.close()
+
+
+def parse_website_with_yellow_lab_tools(url, scrapping_results_dictionary):
+    API_RUNS_ENDPOINT = "https://yellowlab.tools/api/runs"
+    API_RESULT_ENDPOINT = "https://yellowlab.tools/api/results"
+    data = {
+        "url": url
+    }
+    proxy_dict = {
+        "https": "http://159.69.122.69:3128",
+        "http": "http://159.69.122.69:3128"
+    }
+    r = requests.post(url=API_RUNS_ENDPOINT, proxies=proxy_dict, data=data, timeout=30)
+    initial_response = r.json()
+    while(True):
+        running_response = \
+            requests.get(url=API_RUNS_ENDPOINT + f"/{initial_response['runId']}", proxies=proxy_dict, timeout=30).json()
+        if running_response["status"]["statusCode"] == "complete":
+            break
+        time.sleep(3)
+    final_response = requests.get(url=API_RESULT_ENDPOINT + f"/{initial_response['runId']}", proxies=proxy_dict, timeout=30).json()
+
+    scrapping_results_dictionary['ylt_DOMelementsCount'] = final_response['rules']['DOMelementsCount']['value']
+    scrapping_results_dictionary['ylt_DOMelementMaxDepth'] = final_response['rules']['DOMelementMaxDepth']['value']
+    scrapping_results_dictionary['ylt_scriptDuration'] = final_response['rules']['scriptDuration']['value']
+    scrapping_results_dictionary['ylt_jsErrors'] = final_response['rules']['jsErrors']['value']
+    scrapping_results_dictionary['ylt_cssColors'] = final_response['rules']['cssColors']['value']
+    scrapping_results_dictionary['ylt_cssDuplicatedProperties'] = \
+        final_response['rules']['cssDuplicatedProperties']['value']
+    scrapping_results_dictionary['ylt_totalWeight'] = final_response['rules']['totalWeight']['value']
+    scrapping_results_dictionary['ylt_domains'] = final_response['rules']['domains']['value']
+    scrapping_results_dictionary['ylt_compression'] = final_response['rules']['compression']['value']
+    scrapping_results_dictionary['ylt_totalRequests'] = final_response['rules']['totalRequests']['value']
+    scrapping_results_dictionary['ylt_heavyFonts'] = final_response['rules']['heavyFonts']['value']
+    scrapping_results_dictionary['ylt_global_score'] = final_response['scoreProfiles']['generic']['globalScore']
+    scrapping_results_dictionary['ylt_badJavascript_score'] = \
+        final_response['scoreProfiles']['generic']['categories']['badJavascript']['categoryScore']
+    scrapping_results_dictionary['ylt_jQuery_score'] = \
+        final_response['scoreProfiles']['generic']['categories']['jQuery']['categoryScore']
+    scrapping_results_dictionary['ylt_serverConfig_score'] = \
+        final_response['scoreProfiles']['generic']['categories']['serverConfig']['categoryScore']
 
 
 def get_cursor(mysql):
