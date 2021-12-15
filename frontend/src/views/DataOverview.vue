fb9231f5ad291499418cd5839b6846b269785e04
Maciej Czarkowski <mczarkowski98@gmail.com>
2021-12-15 13:14:17 +0100

    Add sorters and fix some layout stuff

--- frontend/src/views/DataOverview.vue
+++ frontend/src/views/DataOverview.vue
@@ -16,13 +16,37 @@
               Website url
             </th>
             <th>
-              Exclamations
+              <div class="flex justify-center">
+                <span>Exclamations</span>
+                <div class="ml-2 text-xl cursor-pointer flex flex-col">
+                  <i :class="{'invisible': !showExclamationsOrderAsc }" @click="sortDescending('exclamation_count')"
+                     class="-mb-3 fas fa-sort-up"></i>
+                  <i :class="{'invisible': !showExclamationsOrderDesc }" @click="sortAscending('exclamation_count')"
+                     class="-mt-1 fas fa-sort-down"></i>
+                </div>
+              </div>
             </th>
             <th>
-              Words to avoid
+              <div class="flex justify-center">
+                <span>Words to avoid</span>
+                <div class="ml-2 text-xl cursor-pointer flex flex-col">
+                  <i :class="{'invisible': !showWordsToAvoidOrderAsc }" @click="sortDescending('words_to_avoid_count')"
+                     class="-mb-3 fas fa-sort-up"></i>
+                  <i :class="{'invisible': !showWordsToAvoidOrderDesc }" @click="sortAscending('words_to_avoid_count')"
+                     class="-mt-1 fas fa-sort-down"></i>
+                </div>
+              </div>
             </th>
             <th>
-              Smart words
+              <div class="flex justify-center">
+                <span>Smart words</span>
+                <div class="ml-2 text-xl cursor-pointer flex flex-col">
+                  <i :class="{'invisible': !showSmartWordsOrderAsc }" @click="sortDescending('smart_words_count')"
+                     class="-mb-3 fas fa-sort-up"></i>
+                  <i :class="{'invisible': !showSmartWordsOrderDesc }" @click="sortAscending('smart_words_count')"
+                     class="-mt-1 fas fa-sort-down"></i>
+                </div>
+              </div>
             </th>
             <th>
               View details
@@ -30,7 +54,7 @@
           </tr>
           </thead>
           <tbody>
-          <tr v-for="(website, index) in websitesData.data" :key="index">
+          <tr v-for="(website, index) in websitesData" :key="index">
             <td>{{ index + 1 }}.</td>
             <td>
               <a :href="website.url" target="_blank" class="hover:text-blue-700">
@@ -41,7 +65,7 @@
             <td>{{ website.words_to_avoid_count }}</td>
             <td>{{ website.smart_words_count }}</td>
             <td class="w-36">
-              <div class="border-2 border-gray-600 rounded-xl cursor-pointer" @click="openWebsiteDataPreview(website)">
+              <div class="border-2 border-gray-600 bg-gray-200 rounded-xl cursor-pointer shadow-xl shadow-inner" @click="openWebsiteDataPreview(website)">
                 Open<i class="ml-2 fas fa-envelope-open-text"></i>
               </div>
             </td>
@@ -49,7 +73,7 @@
           </tbody>
         </table>
       </div>
-      <div class="m-8 p-12 rounded border-2 border-gray-500 text-gray-800" v-if="!websitesData.data.length">
+      <div class="m-8 p-12 rounded border-2 border-gray-500 text-gray-800" v-if="!websitesData.length">
         No entries in databse
       </div>
     </template>
@@ -85,14 +109,19 @@ export default {
       error: null,
       loading: false,
       websiteDataPreviewOpened: false,
-      previewWebsiteData: null
+      previewWebsiteData: null,
+      sortingOrder: {
+        exclamation_count: undefined,
+        smart_words_count: undefined,
+        words_to_avoid_count: undefined
+      }
     }
   },
   mounted() {
     this.loading = true
     axios.get('data').then(
         response => {
-          this.websitesData = response;
+          this.websitesData = response.data;
         },
         error => {
           this.error = error;
@@ -100,6 +129,26 @@ export default {
     ).finally(
         () => this.loading = false
     )
+  },
+  computed: {
+    showExclamationsOrderAsc() {
+      return this.sortingOrder.exclamation_count === 'asc' || this.sortingOrder.exclamation_count === undefined
+    },
+    showExclamationsOrderDesc() {
+      return this.sortingOrder.exclamation_count === 'desc' || this.sortingOrder.exclamation_count === undefined
+    },
+    showWordsToAvoidOrderAsc() {
+      return this.sortingOrder.words_to_avoid_count === 'asc' || this.sortingOrder.words_to_avoid_count === undefined
+    },
+    showWordsToAvoidOrderDesc() {
+      return this.sortingOrder.words_to_avoid_count === 'desc' || this.sortingOrder.words_to_avoid_count === undefined
+    },
+    showSmartWordsOrderAsc() {
+      return this.sortingOrder.smart_words_count === 'asc' || this.sortingOrder.smart_words_count === undefined
+    },
+    showSmartWordsOrderDesc() {
+      return this.sortingOrder.smart_words_count === 'desc' || this.sortingOrder.smart_words_count === undefined
+    }
   },
   methods: {
     openWebsiteDataPreview(website) {
@@ -112,7 +161,38 @@ export default {
       } else {
         return url;
       }
-    }
+    },
+    sortAscending(column) {
+      let setAll = (obj, val) => Object.keys(obj).forEach(k => obj[k] = val);
+      setAll(this.sortingOrder, undefined)
+      console.log("Sorting asc", column)
+      this.sortingOrder[column] = 'asc';
+      this.websitesData.sort((a, b) => {
+        if (a[column] > b[column]) {
+          return 1;
+        }
+        if (a[column] < b[column]) {
+          return -1;
+        }
+        return 0;
+      });
+    },
+
+    sortDescending(column) {
+      let setAll = (obj, val) => Object.keys(obj).forEach(k => obj[k] = val);
+      setAll(this.sortingOrder, undefined)
+      console.log("Sorting desc", column)
+      this.sortingOrder[column] = 'desc';
+      this.websitesData.sort((a, b) => {
+        if (a[column] < b[column]) {
+          return 1;
+        }
+        if (a[column] > b[column]) {
+          return -1;
+        }
+        return 0;
+      });
+    },
   }
 }
 </script>
