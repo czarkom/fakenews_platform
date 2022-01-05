c542e8e9f38957e33f70d722ec8a273c76513faa
TheMioa <49129359+TheMioa@users.noreply.github.com>
2022-01-05 22:49:20 +0100

    Update predicting.py
    Fixing the predicting method

--- predicting.py
+++ predicting.py
@@ -22,29 +22,33 @@ def predict_random(url, path_to_model):
 def predict_from_mock(url, path_to_model):
     f = open('mocked_result.json')
     input_data = json.load(f)
-
+    input_data.pop('url')
     input_data['rating'] = predict_value(input_data, path_to_model)
     return input_data
 
 
 def predict_from_parser(url, path_to_model):
     input_data = websiteParser.parse_website_js(url)
-
+    input_data.pop('url')
     input_data['rating'] = predict_value(input_data, path_to_model)
     return input_data
 
 
-def predict_value(input_data, path_to_model):
+def predict_value_binary(input_data, path_to_model):
     df = pd.DataFrame([input_data])
-    normalized_df = (df - df.min()) / (df.max() - df.min())
-    normalized_df.describe()
+    normalized_df = (df) / float(df.max(axis=1))
     input = normalized_df.values
     model = keras.models.load_model(path_to_model)
-    # jeżeli jest to modelem regresyjnym to tak:
-    # predicted_value = model.predict(input.reshape(-1, 1, 40))*5.
-    # jeżeli jest model klasyfikujący to tak:
     predicted_value = argmax(model.predict(input), axis=-1).astype('int')[0]
     return predicted_value
+
+def predict_value_regression(input_data, path_to_model):
+    df = pd.DataFrame([input_data])
+    normalized_df = (df) / float(df.max(axis=1))
+    input = normalized_df.values
+    model = keras.models.load_model(path_to_model)
+    predicted_value = model.predict(input.reshape(-1, 1, 40))*5.
+    return predicted_value[0][0]
 
 
 def recalculate_model(path_to_model, cursor):
@@ -55,7 +59,7 @@ def recalculate_model(path_to_model, cursor):
     history = model.fit(X, y, validation_split=0.33, epochs=150, batch_size=95)
     # tutaj może jakiś log z dokładnością
     model.save(path_to_model)
-    learningGraph(history)
+    learning_graph(history)
 
 
 def learning_graph(history):
